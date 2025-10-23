0aa118ef7624eeffad0cbb8107f62628707fb3c9
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-10-23 16:28:40 +0300

    app

--- src/app/chances/[id]/page.js
+++ src/app/chances/[id]/page.js
@@ -21,7 +21,7 @@ const Src = item.gallery.map((i) => i.photo_url);
 
 console.log(Src);
 
-console.log(item.categories);
+console.log(item);
 
 const categories = item.categories;
 
@@ -74,6 +74,7 @@ return (
         </div>
       </div>
 <div className="w-full md:w-2/3 m-auto relative bottom-28 p-4">
+
 <div className="flex flex-col md:flex-row gap-8">
     <Slider src={Src} />
 
@@ -97,11 +98,34 @@ return (
         ))}
     </div>
     </div>
+
+    <div className="py-20">
+    <h1 className="text-3xl font-bold text-[#262163]">ابرز المعومات </h1>
+    <div className="w-full h-[2px] bg-gray-300 my-4"></div>
+    <div className="flex justify-between mt-20">
+        <h4 className="text-2xl text-[#262163] font-medium">نوع الاستثمار</h4>
+        <h1 className="text-2xl text-[#262163] font-extrabold">{item.type}</h1>
+    </div>
+    <div className="w-full h-[1px] bg-gray-300 my-4"></div>
+     <div className="flex justify-between mt-8 mb-4">
+        <h4 className="text-2xl text-[#262163] font-medium">مبلغ الاستثمار</h4>
+        <h1 className="text-2xl text-[#262163] font-extrabold">{item.price} ر.س</h1>
+    </div>
+      <div className="w-full h-[1px] bg-gray-300 my-4"></div>
+      <div className="border-2 border-[#262163] p-4 py-8 text-[#262163] bg-[#262163]/15 rounded-4xl my-12">
+يقدّم الرئيس التنفيذي لشركة تدبير عرضًا تفصيليًا في فيديو مدته ١٥ دقيقة، يستعرض فيه استراتيجية الشركة، أبرز الإنجازات، والوضع المالي الحالي.
+      </div>
+    {categories.map((cat, index) => (
+      <div className="mt-28" key={index}>
+      < h1 className="text-2xl font-bold text-[#262163]"> <Image src={cat.icon_url} alt="الخدمات - صورة" width={40} height={40} className="inline-block mr-2" /> {cat.name} </h1>     
+      <div className="w-full h-[2px] bg-gray-300 my-4"></div>
+      <p className="text-lg text-[#262163] font-medium">{cat.description}</p>
+      </div>
+      ))}
 </div>
 
-<div className=" flex flex-col md:flex-row gap-20 py-20 p-4 md:p-8 lg:p-12 xl:p-20">
+</div>
 
-      </div> 
 
     </div>
 
